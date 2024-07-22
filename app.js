07f9dd0b53f256a7df871ee96d91e7d78d7e889f
Daynnight90 <87851186+Daynnight90@users.noreply.github.com>
2024-07-22 16:49:16 +0000

    1

---
+++ app.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const input = document.querySelector(".input")
+const result = document.querySelector(".result")
+const deleteBtn = document.querySelector("delete")
+const keys = document.querySelectorAll("bottom span")
+
+let answer
+let operation = ""
+let decimalAdded = false
+
+const operators = ["+","-","x","/"]
+
+function handleKeyPress(e){}
+const key = e.target.dataset.key
+const lastChar = operation [operation.length - 1]
+
+    if (key === '='){
+    return
+
+    }
+
+    if (key=='.' && decimalAdded){
+        return
+    }
+
+    if(operators.indexOf(key) !== -1){
+        decimalAdded = false
+    }
+
+    if (operation .length === 0 && key === '-') {
+        operation += key
+        input.innerHTML = operation
+        return
+    }
+
+        if (operators.indexOf (lastChar)!== -1 && operators.indexOf (key) !== -1) {\
+        
+            operation =
+        }
